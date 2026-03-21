ae763d471145ddea864aeed67f3054cc595598ed
Michel Gouveia <michel-gouveia1997@hotmail.com>
2026-03-21 07:54:57 -0300

    update

--- dist/appController.js
+++ dist/appController.js
@@ -1,90 +1,9 @@
-/*import { populateCurrencySelect } from "./moedas.js"
-import { CurrencyConverter } from "./currencyConverter.js"
-export function init(){
-
-  const toSelect = document.getElementById('to') as HTMLSelectElement
-  const fromSelect = document.getElementById('from') as HTMLSelectElement
-  
-  const currencies = populateCurrencySelect()
-
-  currencies.forEach(({ code, symbol }) => {
-      const options1 = document.createElement('option')
-
-        options1.value = code
-
-        options1.textContent = `${code} - ${symbol}`
-
-        const options2 = document.createElement('option')
-
-        options2.value = code
-    
-        options2.textContent = `${code} - ${symbol}`
-
-     fromSelect.appendChild(options1)
-     toSelect.appendChild(options2)
-  })
-
-  const convertt = new CurrencyConverter()
-
-   const input = document.getElementById('amount') as HTMLInputElement
-
-    input.value = '0,00'
-
- input.addEventListener('input', (e)=> {
-  const target = e.target as HTMLInputElement;
-
-  const formatter = new Intl.NumberFormat('pt-BR', {
-    minimumFractionDigits: 2,
-    maximumFractionDigits: 2
-  });
-
- 
-  const digits = target.value.replace(/\D/g, '');
-
-  const cents = Number(digits) || 0;
-
-  target.value = formatter.format(cents / 100);
-});
-
-
-   document.getElementById('convert')?.addEventListener('click',async(e)=> {
- 
-    const error = document.getElementById('error') as HTMLParagraphElement
-
-    const to = toSelect.value
-
-    const from = fromSelect.value
-
-    const digits = input.value.replace(/\D/g, '');
-     const amount = Number(digits) / 100;
-
-
-if (isNaN(amount) || amount <= 0) {
-  error.textContent = "Digite um valor válido maior que 0";
-  return;
-}
-
-    error.textContent = '';
-
-    input.value = '0,00'
-
-   const result = await convertt.convert({
-      amount,
-      fromCurrency:from,
-      toCurrency:to
-   });
-
- })
-
-}
-
-document.addEventListener('DOMContentLoaded',init)
-*/
 import { populateCurrencySelect } from "./moedas.js";
 import { CurrencyConverter } from "./currencyConverter.js";
 export function init() {
     const toSelect = document.getElementById('to');
     const fromSelect = document.getElementById('from');
+    const swapBtn = document.getElementById('swap');
     const currencies = populateCurrencySelect();
     currencies.forEach(({ code, symbol }) => {
         const option1 = document.createElement('option');
@@ -136,6 +55,13 @@ export function init() {
             maximumFractionDigits: 2
         }).format(result.convertedAmount);
         input.value = '0,00';
+    });
+    //Problem swap
+    swapBtn?.addEventListener('click', () => {
+        [fromSelect.value, toSelect.value] = [toSelect.value, fromSelect.value];
+        if (input.value !== '0,00') {
+            document.getElementById('convert')?.click();
+        }
     });
 }
 document.addEventListener('DOMContentLoaded', init);
--- src/appController.ts
+++ src/appController.ts
@@ -1,86 +1,3 @@
-/*import { populateCurrencySelect } from "./moedas.js"
-import { CurrencyConverter } from "./currencyConverter.js"
-export function init(){
-
-  const toSelect = document.getElementById('to') as HTMLSelectElement
-  const fromSelect = document.getElementById('from') as HTMLSelectElement
-  
-  const currencies = populateCurrencySelect()
-
-  currencies.forEach(({ code, symbol }) => {
-      const options1 = document.createElement('option')
-
-        options1.value = code
-
-        options1.textContent = `${code} - ${symbol}`
-
-        const options2 = document.createElement('option')
-
-        options2.value = code
-    
-        options2.textContent = `${code} - ${symbol}`
-
-     fromSelect.appendChild(options1)
-     toSelect.appendChild(options2)
-  })
-
-  const convertt = new CurrencyConverter()
-
-   const input = document.getElementById('amount') as HTMLInputElement
-
-    input.value = '0,00'
-
- input.addEventListener('input', (e)=> {
-  const target = e.target as HTMLInputElement;
-
-  const formatter = new Intl.NumberFormat('pt-BR', {
-    minimumFractionDigits: 2,
-    maximumFractionDigits: 2
-  });
-
- 
-  const digits = target.value.replace(/\D/g, '');
-
-  const cents = Number(digits) || 0;
-
-  target.value = formatter.format(cents / 100);
-});
-
-
-   document.getElementById('convert')?.addEventListener('click',async(e)=> {
- 
-    const error = document.getElementById('error') as HTMLParagraphElement
-
-    const to = toSelect.value
-
-    const from = fromSelect.value
-
-    const digits = input.value.replace(/\D/g, '');
-     const amount = Number(digits) / 100;
-
-
-if (isNaN(amount) || amount <= 0) {
-  error.textContent = "Digite um valor válido maior que 0";
-  return;
-}
-
-    error.textContent = '';
-
-    input.value = '0,00'
-
-   const result = await convertt.convert({
-      amount,
-      fromCurrency:from,
-      toCurrency:to
-   });
-
- })
-
-}
-
-document.addEventListener('DOMContentLoaded',init)
-*/
-
 import { populateCurrencySelect } from "./moedas.js"
 import { CurrencyConverter } from "./currencyConverter.js"
 
@@ -88,6 +5,7 @@ export function init(){
 
   const toSelect = document.getElementById('to') as HTMLSelectElement
   const fromSelect = document.getElementById('from') as HTMLSelectElement
+  const swapBtn = document.getElementById('swap') as HTMLButtonElement
   
   const currencies = populateCurrencySelect()
 
@@ -112,7 +30,6 @@ export function init(){
 
   input.value = '0,00'
 
- 
   input.addEventListener('input', (e) => {
     const target = e.target as HTMLInputElement
 
@@ -145,7 +62,6 @@ export function init(){
       res!.textContent = ''
       return
     }
-
     // limpa erro
     error.textContent = ''
 
@@ -160,9 +76,21 @@ export function init(){
   maximumFractionDigits: 2
 }).format(result.convertedAmount)
 
-
     input.value = '0,00'
   })
+
+
+  //Problem swap
+
+swapBtn?.addEventListener('click', () => {
+
+  [fromSelect.value, toSelect.value] = [toSelect.value, fromSelect.value]
+
+  if(input.value !== '0,00'){
+           document.getElementById('convert')?.click()
+  }
+
+})
 }
 
 document.addEventListener('DOMContentLoaded', init)
